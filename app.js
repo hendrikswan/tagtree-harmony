d9c68ee007015a310c0710d826914bc7442720d0
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-05 08:55:50 +0200

    new string interpolation

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ var person =  {
     friends: ['Frikkie', 'Frida']
 };
 
-console.log('%s is friends with the following people: \n\n\t\t -%s \n\n\t\t -%s', 
-        person.name, person.friends[0], person.friends[1]);
+console.log(`${person.name} is friends with the following people:
+     
+     ${person.friends[0]} 
+     
+     ${person.friends[1]}`);
