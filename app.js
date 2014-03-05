81b536f2df48475cbbf5bd9f31eed7fc27d8cc86
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-05 08:43:57 +0200

    old string

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-var hendrik = {
-    name: 'hendrik',
-    friends: [],
-    printFriends: function(){
-        this.friends.forEach((f)=>{
-            console.log('%s is friends with %s', this.name, f);
-        });
-    }
-}
-hendrik.friends.push('Frikkie');
-hendrik.friends.push('Frida');
-hendrik.printFriends();
+console.log('%s is friends with the following people: \n\n\t\t -%s \n\n\t\t -%s', 'hendrik', 'frikkie', 'frida');
