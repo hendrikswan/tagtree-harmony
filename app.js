c224dc05d2073d4780caf77bf668875f70affab7
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-05 08:54:04 +0200

    old string

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('%s is friends with the following people: \n\n\t\t -%s \n\n\t\t -%s', 'hendrik', 'frikkie', 'frida');
+var person =  {
+    name: 'hendrik', 
+    friends: ['Frikkie', 'Frida']
+};
+
+console.log('%s is friends with the following people: \n\n\t\t -%s \n\n\t\t -%s', 
+        person.name, person.friends[0], person.friends[1]);
