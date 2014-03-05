2fb394ab06b6a33897037306f9c502d4d06d2274
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-05 07:47:26 +0200

    literals, string interpolation, arrows

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+var hendrik = {
+    name: 'hendrik',
+    friends: [],
+    printFriends(){
+        this.friends.forEach((f)=>{
+            console.log(`${this.name} is friends with 
 
+                            ${f}
 
+                `);
+        });
+    }
+}
+
+hendrik.friends.push('Frikkie');
+hendrik.friends.push('Frida');
+
+hendrik.printFriends();
