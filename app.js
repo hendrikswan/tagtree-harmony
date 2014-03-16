1ad6cb4a4c7b7bd5dc589904008ebab22664aef3
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-16 22:42:04 +0200

    questions prompting for input, difficult to demo

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-function searchVehicles({make:mk, model:mdl, color:c}){
-    console.log(`searching for vehicles with make ${mk}, model ${mdl} and color ${c}`);
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+class Question {
+
+  constructor(body){
+    this.body = body;
+  }
+
+  ask(){
+    rl.question(this.body, function(answer) {
+      console.log("Thank you for your valuable feedback:", answer);
+      rl.close();
+    });
+  }
+
+  handleAnswer(answer){
+    console.log("Thank you for your valuable feedback:", answer);
+  }
 }
 
-searchVehicles({make: 'Subaru', model: 'Forester', color: 'Gold'});
-
-
-
+var q = new Question('Have you read idiomatic.js on github?');
+q.ask();
