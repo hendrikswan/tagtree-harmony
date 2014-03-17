097261f453b7f7d546abcc778d92beb659958c49
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-17 10:55:01 +0200

    good example of classes and inheritance

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 class Question {
-
   constructor(body){
     this.body = body;
   }
 
   ask(){
-    console.log(this.body);
+    console.log('-> ' + this.body);
   }
 
   answer(answer){
-    console.log(`       --${answer}`);
+    console.log(`       Your answer: ${answer}`);
+  }
+}
+
+
+class MultiChoiceQuestion extends Question {
+  constructor(body, choices){
+    super(body);
+    this.choices = choices;
+  }
+
+  ask() {
+    super();
+    for(var choice of this.choices){
+      console.log( `${this.choices.indexOf(choice) + 1}  - ${choice}`);
+    }
+  }
+
+  answer(answer){
+    answer = this.choices[answer - 1];
+    super(answer);
   }
 }
 
 var q = new Question('Have you read idiomatic.js on github?');
 q.ask();
 q.answer('No, but I heard a lot about it!');
+
+
+var multiQ = new MultiChoiceQuestion('Which tagtree casts have you watched?',
+  ['bla', 'blie']);
+multiQ.ask();
+multiQ.answer(1);
+
+
