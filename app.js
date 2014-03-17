407b3b11a25bed3a4766f8793d4fe90d650c1296
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-17 11:46:50 +0200

    new style literals

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ var master = 'Hendrik';
 
 var doggy = {
   name: 'Frikkie',
-  master: master,
+  master,
   bark: function(){
     console.log('whoohoooooo, wheee whhee whooooooo whooooooooooooooooo');
-  }
+  },
+  ['tick_count_' + new Date().getTime()]:  Math.floor(Math.random() * 10000)
 }
-
-doggy['tick_count_' + new Date().getTime()] = Math.floor(Math.random() * 10000);
 
 doggy.bark();
 console.log(doggy);
