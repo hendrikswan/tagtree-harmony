7247aff087413a99cb140efffa3fd181f1264e0f
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-03-22 14:10:00 +0200

    fixed bug from earlier commit + finished modules sample code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import {findVehicle} from './vehicle';
+import {findVehicle,vehicles} from './vehicle';
 
+console.log(`Here are all the vehicles: ${vehicles}`)
 
 var match = findVehicle({model: 'Focus'});
 console.log(`search result: make: ${match.make}, model: ${match.model}`);
--- vehicle.js
+++ vehicle.js
@@ -1,9 +1,3 @@
-var vehicles = [
-  new Vehicle({make: 'Ford', model: 'Focus'}),
-  new Vehicle({make: 'Subaru',  model: 'Forester'}),
-  new Vehicle({make: 'Honda',   model: 'PCX 125'})
-];
-
 class Vehicle{
   constructor({make, model}){
     this.make = make;
@@ -13,7 +7,18 @@ class Vehicle{
   isMatch(make, model){
     return this.make === make || this.model === model;
   }
+
+  toString(){
+    return ` * ${this.make} ${this.model} `;
+  }
 }
+
+export var vehicles = [
+  new Vehicle({make: 'Ford', model: 'Focus'}),
+  new Vehicle({make: 'Subaru',  model: 'Forester'}),
+  new Vehicle({make: 'Honda',   model: 'PCX 125'})
+];
+
 
 export function findVehicle({make, model}){
   for(var v of vehicles){
