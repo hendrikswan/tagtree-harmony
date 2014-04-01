3f62f795c6507a7ce88562c937bf8a0db1fd6f64
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-04-01 08:11:44 +0200

    new starting point

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-function searchVehicles({make:mk, model:mdl, color:c}){
-    console.log(`searching for vehicles with make ${mk}, model ${mdl} and color ${c}`);
-}
 
-searchVehicles({make: 'Subaru', model: 'Forester', color: 'Gold'});
 
 
 
